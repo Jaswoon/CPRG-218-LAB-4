c1a84a308a9a4e5d767c72e0d02a13a05c1c6404
Jaswoon <147518171+Jaswoon@users.noreply.github.com>
2024-03-07 20:54:17 -0700

    NEW CHANGES
    I MADE A BUNCH OF CHANGES BUT I FORGOT TO MAKE A REPOSITORY FIRST

---
+++ myscript.js
@@ -0,0 +1,8 @@
+function toggleAccordion(button) {
+    var content = button.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  }
